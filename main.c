d16f6198958d3e11528aa06ac011016ddee8d408
AhmedSaeed <ahmed@gg.com>
2024-11-04 11:42:59 +0300

    add main with no change

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;
+    printf("Difference: %d\n", difference);
+
+    return 0;
+}
